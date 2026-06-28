3da39bae6562ed8f20c24b1235c46a2bd9017b4d
Pithani Avinash <Avinash1309a@gmail.com>
2026-06-29 00:07:56 +0530

    Fix page modules

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function AboutPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>About Us</h1>
+
+      <p>
+        Welcome to My Business. We provide quality
+        products and professional services for customers.
+      </p>
+    </div>
+  );
+}
