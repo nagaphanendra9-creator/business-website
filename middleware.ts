a7ad02e4b1faee2ab4534d661726d04249b0354a
Pithani Avinash <Avinash1309a@gmail.com>
2026-06-29 18:30:37 +0530

    Fix middleware export

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,13 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/admin/login"
+  }
+});
 
 export const config = {
-  matcher: ["/admin/:path*"]
+  matcher: [
+    "/admin/:path*",
+  ]
 };
