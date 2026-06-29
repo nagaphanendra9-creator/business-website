f9f5dafdb21cb41e500b383792b6b9c94c73310e
Pithani Avinash <Avinash1309a@gmail.com>
2026-06-29 17:56:19 +0530

    Restore admin login

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,30 @@
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+
 async function main() {
-  console.log("Seed completed");
+
+  const passwordHash = await hash(
+    "Admin@12345",
+    10
+  );
+
+  await prisma.adminUser.upsert({
+    where:{
+      email:"admin@example.com"
+    },
+    update:{},
+    create:{
+      email:"admin@example.com",
+      passwordHash,
+      name:"Admin"
+    }
+  });
+
+  console.log("Admin created");
 }
 
 main()
-  .catch(console.error);
+.catch(console.error)
+.finally(async()=>{
+ await prisma.$disconnect();
+});
